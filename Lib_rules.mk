9073cba4fbbd5bbc99ef89873d2de9227bb0e33b
Max Gautier <mgautier@student.42.fr>
2017-11-11 16:58:10 +0100

    Suppress $(suffix list) for lib

--- Lib_rules.mk
+++ Lib_rules.mk
@@ -6,7 +6,7 @@
 #*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        *#
 #*                                                +#+#+#+#+#+   +#+           *#
 #*   Created: 2017/11/02 14:18:27 by mgautier          #+#    #+#             *#
-#*   Updated: 2017/11/06 11:25:30 by mgautier         ###   ########.fr       *#
+#*   Updated: 2017/11/11 16:57:48 by mgautier         ###   ########.fr       *#
 #*                                                                            *#
 #* ************************************************************************** *#
 
@@ -29,10 +29,10 @@ $(objects): $(obj_dir)/%$(obj_suffix):$(src_dir)/%$(src_suffix) | $(obj_dir)
 endef
 
 define Lib_rule
-suffix_list$1:= $(suffix_list$1) $(shared_lib_suffix) $(static_lib_suffix)
 $(call Lib_rule_specific,$1,$(static_lib_suffix))
 vpath $(TARGET$1).$(static_lib_suffix) $1
 
 endef
+#suffix_list$1:= $(suffix_list$1) $(shared_lib_suffix) $(static_lib_suffix)
 #$(call Lib_rule_specific,$1,$(shared_lib_suffix))
 #vpath $(TARGET$1).$(shared_lib_suffix) $1
