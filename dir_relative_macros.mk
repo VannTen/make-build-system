5eed66f972ac911f792851f38e30abe05d7706fd
Max Gautier <mgautier@student.42.fr>
2017-11-11 16:45:31 +0100

    Fix bug in target dir name for library

--- dir_relative_macros.mk
+++ dir_relative_macros.mk
@@ -6,7 +6,7 @@
 #*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        *#
 #*                                                +#+#+#+#+#+   +#+           *#
 #*   Created: 2017/10/19 17:20:13 by mgautier          #+#    #+#             *#
-#*   Updated: 2017/11/06 11:22:21 by mgautier         ###   ########.fr       *#
+#*   Updated: 2017/11/11 16:45:15 by mgautier         ###   ########.fr       *#
 #*                                                                            *#
 #* ************************************************************************** *#
 
@@ -16,7 +16,7 @@
 # They should be used inside a macro expanded by $call, or directly with it
 
 target = $1$(TARGET$1)
-intermediate_target = $1lib$(TARGET$1).a
+intermediate_target = $1$(if $(findstring lib,$(TARGET$1)),,lib)$(TARGET$1).a
 ext_dependencies = $(intermediate_target)\
   $(foreach sub,$(COMPONENTS$1), $(call ext_dependencies,$(sub)))
 all_includes = $(INC_DIR$1) $(foreach sub,$(SUBDIRS$1),\
